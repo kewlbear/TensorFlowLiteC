530d799c7c56e2e5a3fdaf5e3865e4d121c169ef
Changbeom Ahn <kewlbear@gmail.com>
2021-04-02 15:08:09 +0900

    Update pod.sh

--- Scripts/pod.sh
+++ Scripts/pod.sh
@@ -1,12 +1,14 @@
 V=$1
 
+git fetch --tags
+
 cd App
 
 if [ "$V" = "" ]
 then
   sed s/VERSION//g Podfile.in > Podfile
 else
-  (git fetch --tags | grep "$V") && exit
+  (git tag | grep "$V") && exit
   sed s/VERSION/", '~> $V'"/g Podfile.in > Podfile
 fi
 
@@ -15,7 +17,7 @@ pod install
 if [ "$V" = "" ]
 then
   V=`egrep -o '(\d+\.)+\d+' Podfile.lock | head -1`
-  (git fetch --tags | grep "$V") && exit
+  (git tag | grep "$V") && exit
 fi
 
 echo "::set-output name=version::$V"
